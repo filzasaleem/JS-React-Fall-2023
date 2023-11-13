4e12f0014aadc50b8cffdf32db9f54eb1c12341c
filzasaleem <filza.saleem05@gmail.com>
2023-11-13 12:08:27 +0100

    week5

--- weeks/sprint-2/week-5/exercises/card-component-lab/src/App.jsx
+++ weeks/sprint-2/week-5/exercises/card-component-lab/src/App.jsx
@@ -4,7 +4,10 @@ import Card from "./components/Card";
 function App() {
   return (
     <>
-      <Card />
+        <Card Title="Lemon" SubTitle="This is a really tasty fruit" Icon="🍋" ButtonText="ORDER NOW"  />
+        <Card Title="Rocket" SubTitle="Join us into the outer space!" Icon="🚀" ButtonText="BOOK SEAT"  />
+        <Card Title="Doggy" SubTitle="Hello, do you want to be friends?" Icon="🐶" ButtonText="ADOPT ME"  />
+        
     </>
   );
 }
--- weeks/sprint-2/week-5/exercises/card-component-lab/src/components/Card.jsx
+++ weeks/sprint-2/week-5/exercises/card-component-lab/src/components/Card.jsx
@@ -1,11 +1,47 @@
 import "./Card.css";
 
-const Card = () => {
+const Card = ({ Title, SubTitle, Icon, ButtonText }) => {
   // If the card is clicked, render this:
   // return ();
 
-  // If the card isn't cliked, render this:
-  return <div>Card goes here</div>;
+    
+  const colorTheme = {
+    lemon: {
+      title: "#166F39",
+      primary: "#8CFFBA",
+      secondary: "#CFFFE2",
+      subtitle: "#000000",
+      buttonColor: "#FCED84",
+      buttonTextColor: "#000000",
+    },
+    rocket: {
+      title: "#0B0C11",
+      primary: "#A6CFE2",
+      secondary: "#C2DCE9",
+      subtitle: "#0B0C11",
+      buttonColor: "#0B0C11",
+      buttonTextColor: "#FFD600",
+    },
+    doggy: {
+      title: "#502F7E",
+      primary: "#EEB200",
+      secondary: "#FFE086",
+      subtitle: "#502F7E",
+      buttonColor: "#FFFFFF",
+      buttonTextColor: "#502F7E",
+    }
+  };
+  // If the card isn't clicked, render this:
+  return <div className="CardContainer">
+    <div className="Title">
+      <h1 className="TitleColor">{Title}</h1>  
+      <p>{Icon}</p>
+    </div>
+    <div className="SubContent">
+      <h2 className="SubTitleColor">{SubTitle}</h2>
+      <button className="ButtonColor">{ButtonText}</button>
+    </div>
+    </div>;
 };
 
 export default Card;
